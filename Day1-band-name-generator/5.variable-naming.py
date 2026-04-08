432a8f2f1fba71829652a6a4c8391ca9fa9c0328
Prashant Zunjurke <paysh004@gmail.com>
2026-04-09 01:03:44 +0530

    pending file updated with code

---
+++ Day1-band-name-generator/5.variable-naming.py
@@ -0,0 +1,14 @@
+# varibale naming rules:
+# 1. Variable names can only contain letters, numbers, and underscores.
+# 2. Variable names cannot start with a number.
+# 3. Variable names cannot be the same as Python keywords (e.g., if, else, while, etc.).
+# 4. Variable names should be descriptive and meaningful to make the code easier to read and understand.
+# 5. Variable names are case-sensitive, which means that name and Name are considered different variables.
+# 6. Variable names should not contain spaces. Instead, you can use underscores to separate words (e.g., first_name, last_name).    
+
+
+example_variable_name = "Hello, World!" # it is a valid variable name because it follows all the rules of variable naming
+print(example_variable_name) # it will print Hello, World! because it is a valid variable
+
+# 1st_variable_name = "Hello, World!" # it is an invalid variable name because it starts with a number
+# print(1st_variable_name) # it will raise SyntaxError because variable names cannot start with a number  
